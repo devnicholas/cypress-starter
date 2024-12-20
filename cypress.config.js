04b6edc4f0bea707cb9de34ab4b319f93419d0a2
Nicholas Stefano <nicholas.stefanob@gmail.com>
2024-12-20 18:24:41 -0300

    fix: enabling test isolation

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,6 @@ const { initPlugin } = require("cypress-plugin-snapshots/plugin");
 module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.BASE_URL,
-    testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       initPlugin(on, config);
